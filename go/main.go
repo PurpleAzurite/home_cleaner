3de93833fa27434529b216d8585a3a7f44ceeaeb
PurpleAzurite <misagh.asgari@gmail.com>
2022-09-02 06:20:17 +0430

    Improve control flow

--- go/main.go
+++ go/main.go
@@ -9,7 +9,8 @@ import (
 
 func check(e error) {
 	if e != nil {
-		panic(e)
+		fmt.Println("File not found")
+		os.Exit(1)
 	}
 }
 
@@ -40,32 +41,40 @@ func CheckAndDelete(root string, path string) {
 	}
 }
 
+func PrintUsage() {
+    fmt.Println("Usage: clean [options]\n\t-v, --version\t\tDisplay program version information")
+	fmt.Println("\t-h, --help\t\tDisplay this message\n\t-l, --list <p>\t\tSpecify list file")
+}
+
 func main() {
 	root_dir, _ := os.UserHomeDir()
 	list_path := root_dir + "/.config/clean/files.list"
 
     for idx, val := range os.Args[1:] {
         if val == "-v" || val == "--version" {
-            fmt.Println("GoClean v1.0")
-            return
+            fmt.Println("GoClean v1.0.1")
+			os.Exit(0)
+
         } else if val == "-h" || val == "--help" {
-            fmt.Println("GoClean\nUsage: clean [options]\n\t-v, --version\t\tDisplay program version information")
-            fmt.Println("\t-h, --help\t\tDisplay this message\n\t-l, --list <p>\t\tSpecify list file")
-            return
+			PrintUsage();
+			os.Exit(0)
+
         } else if val == "-l" || val == "--list" {
             if len(os.Args) == idx + 2 {
-                fmt.Println("No file specified.")
-                return
+                fmt.Println("No file specified")
+				os.Exit(0)
             }
 
             next_arg := os.Args[idx + 2]
             if strings.HasPrefix(next_arg, "-") || strings.HasPrefix(next_arg, "--") {
                 fmt.Println("Please specify path to list file")
-                return
+				os.Exit(0)
+
             } else {
                 list_path = next_arg
                 fmt.Printf("Using alternative list file at: %s\n", list_path)
                 break
+
             }
         } else {
             fmt.Printf("Unrecognized option: %s\n", val)
